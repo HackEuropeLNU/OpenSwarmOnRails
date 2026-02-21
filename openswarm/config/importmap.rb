ff5b5a9b64d72e6a2dae775e147894dac4f29112
km222uq <km222uq@student.lnu.se>
2026-02-21 19:42:51 +0100

    pty fix

--- openswarm/config/importmap.rb
+++ openswarm/config/importmap.rb
@@ -1,10 +1,11 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+pin "cable_consumer", to: "cable_consumer.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "@rails/actioncable", to: "https://ga.jspm.io/npm:@rails/actioncable@8.1.201/es2022/actioncable.esm.js"
-pin "xterm", to: "https://cdn.jsdelivr.net/npm/xterm@5.5.0/+esm"
+pin "@rails/actioncable", to: "https://cdn.jsdelivr.net/npm/@rails/actioncable@8.0.100/+esm"
+pin "xterm", to: "https://cdn.jsdelivr.net/npm/@xterm/xterm@5.5.0/+esm"
 pin "@xterm/addon-fit", to: "https://cdn.jsdelivr.net/npm/@xterm/addon-fit@0.10.0/+esm"
 pin_all_from "app/javascript/controllers", under: "controllers"
